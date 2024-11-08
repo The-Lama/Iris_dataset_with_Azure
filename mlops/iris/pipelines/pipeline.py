73e47033052a50c7c6a3fc7d364edfcddd6708ee
Ubuntu <askifimlame@gmail.com>
2024-11-08 13:07:18 +0000

    fixed environment creation and refactored a bit

--- mlops/iris/pipelines/pipeline.py
+++ mlops/iris/pipelines/pipeline.py
@@ -8,8 +8,9 @@ from azure.core.exceptions import ClientAuthenticationError
 from azure.ai.ml import load_component
 from azure.ai.ml import MLClient
 from azure.ai.ml.dsl import pipeline
+from azure.ai.ml.entities import Environment
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 PIPELINE_COMPONENTS = {}
 
@@ -21,47 +22,34 @@ def iris_pipeline():
     train_component()
 
 
-def construct_pipeline():
+def construct_pipeline(cluster_name, environment):
     """Construct the iris pipeline by loading the components."""
     logging.debug("loading pipeline components...")
     components_dir = Path("mlops/iris/components")
-    train_model = load_component(source=components_dir / "train.yml")
+    train_component = load_component(source=components_dir / "train.yml")
+    train_component.environment = environment
 
     logging.debug("Constructing pipeline...")
-    PIPELINE_COMPONENTS["train"] = train_model
+    PIPELINE_COMPONENTS["train"] = train_component
 
     pipeline_job = iris_pipeline()
+    pipeline_job.compute = cluster_name
 
     return pipeline_job
 
 
-def execute_pipeline(
-    subscription_id, resource_group_name, workspace_name, pipeline_job
-):
+def execute_pipeline(client, pipeline_job):
     """Execute the Iris pipeline."""
-    try:
-        ml_client = MLClient(
-            credential=DefaultAzureCredential(),
-            subscription_id=subscription_id,
-            resource_group_name=resource_group_name,
-            workspace_name=workspace_name,
-        )
+    logging.debug("Submitting pipeline job...")
+    pipeline_job = client.jobs.create_or_update(
+        pipeline_job, experiment_name="ML_with_Iris_Dataset"
+    )
 
-        logging.debug("Submitting pipeline job...")
-        pipeline_job = ml_client.jobs.create_or_update(
-            pipeline_job, experiment_name="ML with Iris Dataset."
-        )
-
-        logging.info(f"{pipeline_job.name} has been submitted")
-
-    except ClientAuthenticationError as e:
-        logging.error("Invalid credentials.. try again")
-        logging.error(f"Authentication failed: {e.message}")
+    logging.info(f"{pipeline_job.name} has been submitted")
 
 
-if __name__ == "__main__":
-    load_dotenv()
-
+def load_configuration():
+    """Load configuration from environment variables or command-line arguments."""
     parser = argparse.ArgumentParser("pipeline")
     parser.add_argument(
         "--subscription_id",
@@ -81,13 +69,44 @@ if __name__ == "__main__":
         default=os.getenv("WORKSPACE_NAME"),
         help="Azure Machine Learning workspace name",
     )
-
-    args = parser.parse_args()
-
-    pipeline_job = construct_pipeline()
-    execute_pipeline(
-        args.subscription_id,
-        args.resource_group_name,
-        args.workspace_name,
-        pipeline_job,
+    parser.add_argument(
+        "--cluster_name",
+        type=str,
+        default=os.getenv("CLUSTER_NAME"),
+        help="Name of azure compute cluster",
     )
+    parser.add_argument(
+        "--env_base_image_name",
+        type=str,
+        default=os.getenv("ENV_BASE_IMAGE_NAME"),
+        help="Name of base image for system managed environment",
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    args = load_configuration()
+
+    try:
+        client = MLClient(
+            DefaultAzureCredential(),
+            subscription_id=args.subscription_id,
+            resource_group_name=args.resource_group_name,
+            workspace_name=args.workspace_name,
+        )
+    except ClientAuthenticationError as e:
+        logging.error("Invalid credentials.. try again")
+        logging.error(f"Authentication failed: {e.message}")
+
+    environment = Environment(
+        image=args.env_base_image_name,
+        conda_file="mlops/iris/environments/ml-environment.yml",
+        name="iris-ml",
+        description="environment to run the ml code",
+    )
+    environment = client.environments.create_or_update(environment)
+
+    pipeline_job = construct_pipeline(args.cluster_name, environment)
+    execute_pipeline(client, pipeline_job)

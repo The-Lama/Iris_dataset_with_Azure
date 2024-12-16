e1ae6064a24e0af30a39c7933855b094d16cd089
Ubuntu <askifimlama@gmail.com>
2024-12-16 09:17:09 +0000

    added mlflow to save and log the model.

--- mlops/iris/src/train.py
+++ mlops/iris/src/train.py
@@ -1,5 +1,9 @@
 import argparse
+import joblib
 import logging
+import mlflow
+import mlflow.sklearn
+import pandas as pd
 from pathlib import Path
 from sklearn.linear_model import LinearRegression
 
@@ -8,27 +12,39 @@ import utils
 logging.basicConfig(level=logging.INFO)
 
 
+def load_training_data(data_path: str) -> tuple[pd.DataFrame, pd.Series]:
+    """Load the training data from the directory containing train.csv and test.csv."""
+    train_data, _ = utils.load_train_and_test_data(data_path)
+
+    X_train = utils.get_features(train_data)
+    y_train = utils.get_targets(train_data)
+
+    return X_train, y_train
+
+
 def train_model(transformed_data_path: str, model_output_path: str):
     """Train the model with the provided data."""
     logging.info("Starting model training.")
     logging.debug(f"Transformed data path: {transformed_data_path}")
     logging.debug(f"Model output path: {model_output_path}")
 
+    mlflow.autolog()
+
+    X_train, y_train = load_training_data(transformed_data_path)
+
     model_output_path = Path(model_output_path)
     model_output_path.mkdir(parents=True, exist_ok=True)
 
-    train_data, test_data = utils.load_train_and_test_data(transformed_data_path)
+    with mlflow.start_run():
+        model = LinearRegression()
+        model.fit(X_train, y_train)
 
-    X_train = utils.get_features(train_data)
-    y_train = utils.get_targets(train_data)
+        training_score = model.score(X_train, y_train)
+        logging.info(f"Model score: {training_score}")
 
-    X_test = utils.get_features(test_data)
-    y_test = utils.get_targets(test_data)
-
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-    logging.info("Model trained sucessfully.")
-    logging.info(f"Model score: {model.score(X_test, y_test)}")
+        model_file_path = model_output_path / "model.joblib"
+        joblib.dump(model, model_file_path)
+        logging.info(f"Model saved to {model_file_path}")
 
 
 if __name__ == "__main__":
